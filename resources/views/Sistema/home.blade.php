efed394b11596c87285813cbb8ad3e2f72198581
lucamba capingana <sabino@gmail.com>
2021-02-25 01:22:04 +0100

    projecto principal

--- resources/views/Sistema/home.blade.php
+++ resources/views/Sistema/home.blade.php
@@ -1,26 +1,4 @@
 @extends('sistema.admin.templates.pagina2')
 @section('conteudo')
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>user</th>
-                <th>name</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td scope="row"></td>
-                <td>1</td>
-                <td>cap</td>
-                <td>a</td>
-            </tr>
-            <tr>
-                <td scope="row"></td>
-                <td>2</td>
-                <td>sa</td>
-                <td>b</td>
-            </tr>
-        </tbody>
-    </table>
+    conteudo da página
 @endsection
